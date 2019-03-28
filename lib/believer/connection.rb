9475e43db6b62c48107365a1ca27e5e497c3b3c6
Bram Nijmeijer <bram.nijmeijer@nedap.com>
2019-03-28 13:22:12 +0100

    There is already a default config

--- lib/believer/connection.rb
+++ lib/believer/connection.rb
@@ -24,12 +24,6 @@ module Believer
       def initialize
         env = Believer::Base.environment
         pool_config = env.connection_pool_configuration
-        if pool_config.nil?
-          pool_config = {
-              :size => 1,
-              :timeout => 10
-          }
-        end
         @connection_pool = ConnectionPool.new(pool_config.symbolize_keys) do
           env.create_connection(:connect_to_keyspace => true)
         end
